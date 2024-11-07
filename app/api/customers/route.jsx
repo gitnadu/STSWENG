63750ad314c71f2c8325d6a25d2ec3f3b0ed356e
AndreiPalonpon <andrei_palonpon@dlsu.edu.ph>
2024-11-07 18:42:16 +0800

    Add an API to delete a selected customer

--- app/api/customers/route.jsx
+++ app/api/customers/route.jsx
@@ -91,6 +91,27 @@ export async function PUT() {
 
 }
 
-export async function DELETE() {
+export async function DELETE(request) {
+    try {
+        const { customer_id } = await request.json();
 
+        //Checks whether there are forms of the selected customer.
+        //If there are forms, throw an error.
+
+        //Deletes the selected customer.
+        await Customer.deleteOne({ _id: customer_id });
+
+        //Returns a response.
+        console.log(`Customer with id ${customer_id} has been deleted successfully.`);
+        return Response.json({ 
+            message: `Customer with id ${customer_id} has been deleted successfully.`, 
+            status: 200 
+        });
+    } catch (error) {
+        console.log("Error: ", error);
+        return Response.json({ 
+            message: "Error occured while deleting a selected customer instance.", 
+            status: 500 
+        });
+    }
 }
